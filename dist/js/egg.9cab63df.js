1ae92049b710b30bc2a8adc09b9c80a5935caaa5
la vie est belle <louasure@126.com>
2021-11-23 10:44:00 +0800

    Delete dist directory

--- dist/js/egg.9cab63df.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["egg"],{"185b":function(n,o,s){"use strict";s.r(o);const w={};o["default"]=w}}]);
-//# sourceMappingURL=egg.9cab63df.js.map
